3b7ab3ec5c537881658868a7836476d52beb881d
Dnyaneshwar <dnyaneshrt>
2021-10-15 19:38:11 +0530

    path change from whatsApp to camera and it's working now.

--- app/src/main/java/com/tech/rectest/MainActivity.kt
+++ app/src/main/java/com/tech/rectest/MainActivity.kt
@@ -4,30 +4,33 @@ import android.Manifest
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.recyclerview.widget.RecyclerView
 import java.io.File
 
 class MainActivity : AppCompatActivity() {
+    var my_recycler_view:RecyclerView?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        my_recycler_view=findViewById(R.id.my_recycler_view)
 
         var status =
             ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
 
 
-        if (status == PackageManager.PERMISSION_GRANTED ) {
+        if (status == PackageManager.PERMISSION_GRANTED) {
             getData()
         } else {
             ActivityCompat.requestPermissions(
                 this,
-                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
-                34
+                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE),
+                22
             )
         }
 
@@ -36,26 +39,23 @@ class MainActivity : AppCompatActivity() {
     //getting data from internal storage(permission)
     private fun getData() {
 
-        var path="/storage/emulated/0/WhatsApp/Media/WhatsAppImages/"
-        var f= File(path)
-        if(!f.exists())
-        {
-            var path="/storage/SdCard0/WhatsApp/Media/WhatsAppImages/"
-            var f= File(path)
+        var path ="/storage/emulated/0/DCIM/camera/"
+        var f = File(path)
+        if (!f.exists()) {
+            path ="/storage/SdCard0/DCIM/camera/"
+            f = File(path)
         }
-        var files_list= f.listFiles()  // data is available here in the form of array of files
+        var files_list = f.listFiles()  // data is available here in the form of array of files
 
 
         //stpe 5: add the layout manager
 
-        my_recycler_view.layoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
-
+        my_recycler_view?.layoutManager=LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
         //activity is sending data to recycler View from here
-        var adapter = MyAdapter(this, files_list)
+        var myAdapter=MyAdapter(this,files_list)
 
         //set Adapter
-        my_recycler_view.adapter = adapter
-
+        my_recycler_view?.adapter=myAdapter
 
     }
 
--- app/src/main/java/com/tech/rectest/MyAdapter.kt
+++ app/src/main/java/com/tech/rectest/MyAdapter.kt
@@ -1,58 +1,58 @@
 package com.tech.rectest
 
+
 import android.graphics.BitmapFactory
 import android.media.ThumbnailUtils
+import android.util.Log
+
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
+import android.widget.*
 import androidx.recyclerview.widget.RecyclerView
 import de.hdodenhof.circleimageview.CircleImageView
+
 import java.io.File
 
 //step 1: create a class as a child of RecyclerView.Adapter<MyViewHolder>
 //step 3: provide implemenation to abstract methods.(present in RecyclerView.Adapter)
 
-class MyAdapter(var mainActivity: MainActivity, var listfile: Array<File>) :
-    RecyclerView.Adapter<MyViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        var inflater = LayoutInflater.from(parent.context)
-        var view = inflater.inflate(R.layout.my_view, parent, false)
-        var Holder = MyViewHolder(view)
-        return Holder
-    }
+class MyAdapter(var mainActivity: MainActivity, var listfiles: Array<File>?) : RecyclerView.Adapter<MyAdapter.MyHolder>() {
 
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        var file_item = listfile[position]
 
-        var btmp = BitmapFactory.decodeFile(file_item.path)
-        var cmp_image = ThumbnailUtils.extractThumbnail(btmp, 50, 50)
 
-        holder.circleImageView.setImageBitmap(cmp_image)
-        
-        holder.text_name.text = file_item.name
-        holder.text_size.text = file_item.length().toString()
-        holder.btn_delete.setOnClickListener {
-            //use AlertDialog before deleting it.
-            file_item.delete()
+        class MyHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            var circle_image = itemView.findViewById<CircleImageView>(R.id.circle_image)
+            var txt_name = itemView.findViewById<TextView>(R.id.txt_name)
+            var txt_size = itemView.findViewById<TextView>(R.id.txt_size)
+            var button_delete = itemView.findViewById<ImageView>(R.id.btn_delete)
+
         }
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyHolder {
+
+        var inflater = LayoutInflater.from(parent.context)
+        var view = inflater.inflate(R.layout.my, parent, false)
+        return MyHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MyHolder, position: Int) {
+
+        var file = listfiles!![position]
+        var bmp = BitmapFactory.decodeFile(file.path)
+        var cmp = ThumbnailUtils.extractThumbnail(bmp, 110, 110)
+        holder.circle_image.setImageBitmap(cmp)
+
+        holder.txt_name.text = file.name
+        holder.txt_size.text = file.length().toString()
+        holder.button_delete.setOnClickListener {
+            file.delete()
+        }
     }
 
     override fun getItemCount(): Int {
-        return listfile.size
+        Log.d("count",listfiles!!.size.toString())
+        return listfiles!!.size
     }
 }
 
-
-//Step 2: create a ViewHolder class as a child of  RecyclerView.ViewHolder(itemView)
-//ste 4: collect your views inside ViewHolder
-class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    var circleImageView = itemView.findViewById<CircleImageView>(R.id.circle_image)
-    var text_name = itemView.findViewById<TextView>(R.id.txt_name)
-    var text_size = itemView.findViewById<TextView>(R.id.txt_size)
-    var btn_delete = itemView.findViewById<Button>(R.id.btn_delete)
-
-
-}
